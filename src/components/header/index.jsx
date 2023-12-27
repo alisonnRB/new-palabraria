90bdca55a460cb7f617b8af853aedb760d2c9027
alisonnRB <alisonrbjs@gmail.com>
2023-12-26 22:35:30 -0300

    animation config

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -3,13 +3,25 @@ import React, { useState } from "react";
 import logo from '../../drawble/abacaxiWhite.png';
 import lupa from '../../drawble/lupa.png';
 import config from '../../drawble/configuracoes.png';
+import { Link } from "react-router-dom";
 
 export default function Header() {
     const [configuration, setConfigaration] = useState(false);
+    const [animationConfig, setAnimationConfig] = useState(false);
+
+    const handleClose = () => {
+        setAnimationConfig(true);
+    
+        const timeoutId = setTimeout(() => {
+            setAnimationConfig(false);
+            setConfigaration(false);
+            clearTimeout(timeoutId);
+        }, 300);
+    };
 
     return (
         <header className="header">
-            <img src={config} alt="configurações" className="config" />
+            <img src={config} alt="configurações" className="config" onClick={()=>{setConfigaration(true)}}/>
 
             <img src={logo} alt="logotipo palabraria" className="logo" />
 
@@ -20,8 +32,14 @@ export default function Header() {
 
 
             {configuration ?
-                <div className="aba-config">
-
+                <div className="content-aba-config" onClick={()=>{handleClose()}}> 
+                    <nav className={`aba-config ${animationConfig ? 'open' : ''}`} onClick={(e)=>{e.stopPropagation()}}>
+                        <Link ><p className="rote">LOGIN</p></Link>
+                        <Link ><p className="rote">CADASTRAR PALAVRAS</p></Link>
+                        <Link ><p className="rote">REQUIRIR UM LOGIN</p></Link>
+                        <Link ><p className="rote">MODERAÇÃO</p></Link>
+                        <Link ><p className="rote">LOGOUT</p></Link>
+                    </nav>
                 </div>
                 : null}
         </header>
