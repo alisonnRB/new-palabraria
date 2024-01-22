020104a787ef58005560965bf578fad6e3cd4f34
alisonnRB <alisonrbjs@gmail.com>
2024-01-22 15:24:42 -0300

    styled path of logins

--- src/page/login/index.jsx
+++ src/page/login/index.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 
+import logo from '../../drawble/abacaxiBlack.png'
 
-export default function Login(){
-    return(
+export default function Login() {
+
+    return (
         <div className="content-login">
+            <div className="card-login">
+                <div className="logo">
+                    <img src={logo} alt="" />
+                </div>
+                <form className="content">
+                    <span className="input-box">
+                        <label htmlFor="user">USER:</label>
+                        <input type="text" id="user" />
+                    </span>
 
+                    <span className="input-box">
+                        <label htmlFor="senha">SENHA:</label>
+                        <input type="password" id="senha" />
+                    </span>
+
+                    <p className="error"></p>
+
+                    <input type="submit" value='ENTRAR' className="bt"/>
+                </form>
+            </div>
         </div>
     )
 }
