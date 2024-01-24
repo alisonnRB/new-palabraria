eb5a186322f0304c5b5884d2d55ecb767a835b27
alisonnRB <alisonrbjs@gmail.com>
2024-01-24 10:58:05 -0300

    cadastro de usuarios

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './output/index.css';
 
 import HomePage from "./page/homepage/index";
 import Login from "./page/login";
+import CadastrarUser from "./page/cadastrarUser";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
             <Route exact path='/*' element={ <HomePage/> } />
             <Route exact path='/Login' element={ <Login/> } />
+            <Route exact path='/cadastro-user' element={ <CadastrarUser/> } />
         </Routes>
     </Router>
   );
---
+++ src/page/cadastrarUser/index.jsx
@@ -0,0 +1,96 @@
+import React, { useState, useLayoutEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+import logo from '../../drawble/abacaxiBlack.png'
+
+export default function CadastrarUser() {
+    const navigate = useNavigate();
+
+    const [permitido, setPermitido] = useState(false);
+
+    const [user, setUser] = useState('');
+    const [senha, setSenha] = useState('');
+    const [permissao, setPermissao] = useState('');
+    const [erro, setErro] = useState('');
+
+    const Verify_Auth = async (token) => {
+        try {
+            const response = await axios.post('http://localhost/src/controls/login.php', {}, {
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'application/json',
+                }
+            });
+
+            if (response.data.ok) {
+                setPermitido(true);
+            } else {
+                navigate(-1);
+            }
+
+        } catch (error) {
+            console.error('Erro ao enviar requisição:', error);
+            navigate(-1);
+        }
+    };
+
+
+    const Cadastrar = async (event) => {
+        event.preventDefault();
+
+        try {
+            const response = await axios.post('http://localhost/src/controls/login.php', {
+                user: user,
+                senha: senha,
+                permissao: permissao
+            });
+            if (!response.data.ok) {
+                setErro(response.data.response);
+            }
+        } catch (error) {
+            console.error('Erro ao enviar requisição:', error);
+        }
+    };
+
+    useLayoutEffect(() => {
+        const token = sessionStorage.getItem('token');
+        Verify_Auth(token);
+    }, []);
+
+
+    return (
+        <div className="content-login">
+            {permitido ? <div className="card-login">
+                <div className="logo">
+                    <img src={logo} alt="logo do projeto palabraria" />
+                </div>
+
+                <span className="title">
+                    <p>CADASTRAR NOVO USUÁRIO</p>
+                </span>
+
+                <form className="content cad" onSubmit={(e) => { Cadastrar(e) }}>
+                    <span className="input-box">
+                        <input type="text" id="user" value={user} onChange={(e) => { setUser(e.target.value) }} placeholder="User..." />
+                    </span>
+
+                    <span className="input-box">
+                        <input type="password" id="senha" value={senha} onChange={(e) => { setSenha(e.target.value) }} placeholder="Senha..." />
+                    </span>
+
+                    <select className="selection" value={permissao} onChange={(e) => { setPermissao(e.target.value) }}>
+                        <option value="" selected>PERMISSÃO</option>
+                        <option value="admin">ADMIN</option>
+                        <option value="moderador">MODERADOR</option>
+                        <option value="instituicao">INSTITUIÇÂO</option>
+                    </select>
+
+                    <p className="error">{erro}</p>
+
+                    <input type="submit" value='CADASTRAR' className="bt cad" />
+                </form>
+            </div> : null}
+        </div>
+    )
+}
