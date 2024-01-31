b8676952751b476b96daa226a8dc6e245166eedb
alisonnRB <alisonrbjs@gmail.com>
2024-01-31 18:06:12 -0300

    debouce created

--- src/page/cadastrar-palavra/form/form1.jsx
+++ src/page/cadastrar-palavra/form/form1.jsx
@@ -1,11 +1,46 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function FormulareUM(props) {
-
     const [ENpalavra, setENpalavra] = useState('');
     const [PTpalavra, setPTpalavra] = useState('');
     const [desc, setDesc] = useState('');
+
+    const [erro, setErro] = useState(false);
+    const [erromsg, setErroMsg] = useState('');
+
+    const [searchTerm, setSearchTerm] = useState('');
+
+    useEffect(() => {
+        const searchDebounced = async () => {
+            const token = sessionStorage.getItem('token');
+            try {
+                const response = await axios.get(`http://localhost/src/controls/palavra-existe.php?palavra=${searchTerm}`,
+                    {
+                        headers: {
+                            'Authorization': `Bearer ${token}`,
+                            'Content-Type': 'application/json',
+                        }
+                    });
+
+                if (response.data.ok) {
+                    setErro(true);
+                    setErroMsg(response.data.response);
+                } else {
+                    setErro(false);
+                    setErroMsg(response.data.response);
+                }
+            } catch (error) {
+                console.error('Erro ao enviar requisição:', error);
+            }
+        };
+
+        const debounceTimer = setTimeout(() => {
+            searchDebounced();
+        }, 500);
+
+        return () => clearTimeout(debounceTimer);
+    }, [searchTerm]);
 
     const complete = () => {
         props.form1.current = {
@@ -15,7 +50,10 @@ export default function FormulareUM(props) {
         }
     }
 
-
+    const handleInputChange = (e) => {
+        setENpalavra(e.target.value);
+        setSearchTerm(e.target.value);
+    }
 
     return (
         <div className="box-form">
@@ -23,9 +61,9 @@ export default function FormulareUM(props) {
                 <span>
                     <span className="label-error">
                         <label htmlFor="ESpalavra">Palavra em espanhol:</label>
-                        <p>* essa palavra já foi catalogada</p>
+                        {erromsg && ENpalavra ? <p style={erro ? { color: 'green' } : { color: 'red' }}>* {erromsg}</p> : null}
                     </span>
-                    <input type="text" id="ESpalavra" value={ENpalavra} onChange={(e) => { setENpalavra(e.target.value) }} />
+                    <input type="text" id="ESpalavra" value={ENpalavra} onChange={handleInputChange} />
                 </span>
 
                 <span>
@@ -43,8 +81,8 @@ export default function FormulareUM(props) {
 
             <p
                 className="prox-bt"
-                style={desc && PTpalavra && ENpalavra ? { color: "black" } : { color: "grey" }}
-                onClick={desc && PTpalavra && ENpalavra ? () => { complete(); props.setEtapa(2)} : ()=>{complete()}}
+                style={desc && PTpalavra && ENpalavra && erro ? { color: "black" } : { color: "grey" }}
+                onClick={desc && PTpalavra && ENpalavra && erro ? () => { complete(); props.setEtapa(2) } : () => { complete() }}
             >
                 CONTINUAR
             </p>
