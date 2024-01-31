a61d03a5791fe2e01025bc039dc48ab9b2a7739e
alisonnRB <alisonrbjs@gmail.com>
2024-01-31 17:14:43 -0300

    formularie complete

--- src/page/cadastrar-palavra/form/form3.jsx
+++ src/page/cadastrar-palavra/form/form3.jsx
@@ -1,10 +1,54 @@
-import React from "react";
+import React, {useState} from "react";
 
 
-export default function FormulareTRES(){
-    return(
-        <div className="box-form"> 
-            
+export default function FormulareTRES(props) {
+    const [transcricao, setTranscricao] = useState();
+
+    const [expressao1, setExpressao1] = useState('');
+    const [expressao2, setExpressao2] = useState('');
+    const [expressao3, setExpressao3] = useState('');
+    const [expressao4, setExpressao4] = useState('');
+
+    const Finalizar = () => {
+        props.form3.current = {
+            'transcricao': transcricao,
+            'expressao1': expressao1,
+            'expressao2': expressao2,
+            'expressao3': expressao3,
+            'expressao4': expressao4,
+        }
+    }
+
+    return (
+        <div className="box-form tres">
+            <div className="content-transc">
+                <span className="content-label">
+                    <label htmlFor="transcri">Transcricão fonética:</label>
+                    <input type="text" id="transcri" value={transcricao} onChange={(e)=>{setTranscricao(e.target.value)}}/>
+                </span>
+            </div>
+
+            <label htmlFor="expre" id="label-ex">Expressões idiomaticas: </label>
+
+            <div className="content-expres">
+
+                <input type="text" id="expre" value={expressao1} onChange={(e)=>{setExpressao1(e.target.value)}}/>
+
+                <input type="text" value={expressao2} onChange={(e)=>{setExpressao2(e.target.value)}}/>
+
+                <input type="text" value={expressao3} onChange={(e)=>{setExpressao3(e.target.value)}}/>
+
+                <input type="text" value={expressao4} onChange={(e)=>{setExpressao4(e.target.value)}}/>
+
+            </div>
+
+            <p
+                className="prox-bt"
+                style={{color: 'black'}}
+                onClick={()=>{Finalizar()}}
+            >
+                FINALIZAR
+            </p>
         </div>
     );
 }
--- src/page/cadastrar-palavra/index.jsx
+++ src/page/cadastrar-palavra/index.jsx
@@ -1,13 +1,15 @@
-import React, {useState, useRef} from "react";
+import React, { useState, useRef } from "react";
 
 import Form1 from './form/form1';
 import Form2 from './form/form2';
+import Form3 from './form/form3';
 
-export default function CadastrarPalavra() {
+export default function CadastrarPalavra(props) {
     const [etapa, setEtapa] = useState(1);
     const form1 = useRef();
     const form2 = useRef();
-    
+    const form3 = useRef();
+
 
     return (
         <div className="content-forms">
@@ -16,25 +18,34 @@ export default function CadastrarPalavra() {
 
                 <span className="box">
                     <p className="etapa">ETAPA 1</p>
-                    <div className="etapa-box um" style={etapa == 1 ? {background: 'black'} : {background: 'grey'}}></div>
+                    <div className="etapa-box um" style={etapa == 1 ? { background: 'black' } : { background: 'grey' }}></div>
                 </span>
 
                 <span className="box">
                     <p className="etapa">ETAPA 2</p>
-                    <div className="etapa-box" style={etapa == 2 ? {background: 'black'} : {background: 'grey'}}></div>
+                    <div className="etapa-box" style={etapa == 2 ? { background: 'black' } : { background: 'grey' }}></div>
                 </span>
 
                 <span className="box">
                     <p className="etapa">ETAPA 3</p>
-                    <div className="etapa-box dois" style={etapa == 3 ? {background: 'black'} : {background: 'grey'}}></div>
+                    <div className="etapa-box dois" style={etapa == 3 ? { background: 'black' } : { background: 'grey' }}></div>
                 </span>
 
             </span>
 
             <form className="forms">
-                {etapa == 1 ? <Form1 setEtapa={setEtapa} form1={form1}/> : null}
-                {etapa == 2 ? <Form2 setEtapa={setEtapa} form2={form2}/> : null}
+                {etapa == 1 ? <Form1 setEtapa={setEtapa} form1={form1} /> : null}
+                {etapa == 2 ? <Form2 setEtapa={setEtapa} form2={form2} /> : null}
+                {etapa == 3 ? <Form3 setEtapa={setEtapa} form3={form3} /> : null}
             </form>
+
+            <p
+                className="prox-bt"
+                style={{ color: 'black' }}
+                onClick={() => { props.setEtapa(prevState => prevState + 1) }}
+            >
+                CONTINUAR
+            </p>
         </div>
     );
 }
