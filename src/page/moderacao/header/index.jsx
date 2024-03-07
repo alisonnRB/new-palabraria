a58f60e32f1e930d50bc265f3d72cb803cc70888
alisonnRB <alisonrbjs@gmail.com>
2024-03-07 09:40:56 -0300

    search words in moder mode created

--- src/page/moderacao/header/index.jsx
+++ src/page/moderacao/header/index.jsx
@@ -2,14 +2,15 @@ import React from "react";
 
 export default function Header(props){
 
+
     return(
         <header className="header-moder">
-            <select className="bd-type" name="bd-type" id="bd-type" value={props.busca} onChange={(e)=>{props.setBusca(e.target.value)}}>
+            <select className="bd-type" name="bd-type" id="bd-type" value={props.bd} onChange={(e)=>{props.setBd(e.target.value)}}>
                 <option value="mod">Moderação</option>
                 <option value="geral">Geral</option>
             </select>
 
-            <input type="text" className="search" placeholder="search. . . ." value={props.bd} onChange={(e)=>{props.setBd(e.target.value)}}/>
+            <input type="text" className="search" placeholder="search. . . ." value={props.busca} onChange={(e)=>{props.setBusca(e.target.value)}}/>
 
             <div className="not-box"></div>
         </header>
--- src/page/moderacao/index.jsx
+++ src/page/moderacao/index.jsx
@@ -1,19 +1,115 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { useInView } from "react-intersection-observer";
+import axios from "axios";
 
 import Header from "./header";
 import CardWord from "../../components/cardWord/index"
 
 
-export default function Moder(){
+export default function Moder() {
     const [busca, setBusca] = useState('');
-    const [bd, setBd] = useState('');
+    const [bd, setBd] = useState('mod');
 
-    return(
+    const index = useRef(0);
+    const [ref, inView] = useInView();
+    const [load, setLoad] = useState(false);
+    const [word, setWord] = useState([]);
+
+    const [noMOre, setNoMore] = useState(false);
+    const [notHave, setNotHave] = useState(false)
+    
+    const searchDebounced = async (type = false) => {
+        try {
+
+            const response = await axios.get(`http://localhost/src/controls/search.php?tipo=moder&busca=${busca}&index=${index.current}&bd=${bd}`,
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                    }
+                });
+
+            if (type) {
+                if (response.data.ok) {
+                    setWord((prevState) => [
+                        ...prevState,
+                        ...response.data.response
+                    ]);
+                } else {
+                    setNoMore(true);
+                }
+
+            } else {
+                if (response.data.ok) {
+                    setNotHave(false);
+                    setWord(response.data.response);
+                } else {
+                    setWord([]);
+                    setNotHave(true);
+                }
+            }
+
+
+        } catch (error) {
+            console.error('Erro ao enviar requisição:', error);
+        }
+
+        setLoad(false);
+
+    };
+
+
+    useEffect(() => {
+        const debounceTimer = setTimeout(() => {
+            index.current = 0;
+            setNoMore(false);
+            setLoad(false);
+            searchDebounced();
+        }, 500);
+
+        return () => clearTimeout(debounceTimer);
+    }, [busca, bd]);
+
+    useEffect(() => {
+        if (load) {
+            return;
+        }
+
+        if (inView) {
+            setLoad(true);
+            index.current += 6;
+            searchDebounced(true);
+        }
+    }, [inView])
+
+    const geraCard = () => {
+        const list = [];
+
+        for (let i = 0; i < Object.keys(word).length; i++) {
+            const iten = <CardWord key={i} infos={word[i]} />
+            list.push(iten);
+        }
+
+        return list;
+    }
+
+
+    return (
         <div className="content-moder">
-            <Header busca={busca} setBusca={setBusca} bd={bd} setBd={setBd}/>
+            <Header busca={busca} setBusca={setBusca} bd={bd} setBd={setBd} />
 
             <div className="search-box">
-                <CardWord />
+                {geraCard()}
+
+
+                {notHave ? <p className="result">NÂO HÁ RESULTADOS PARA ESSA BUSCA!</p> : null}
+
+                {noMOre && !notHave ? <p className="result">NÃO HÁ MAIS RESULTADOS!</p> : null}
+
+                {load ? <div className="c-loader"></div> : null}
+
+                <div ref={ref} className="view">
+
+                </div>
             </div>
         </div>
     );
